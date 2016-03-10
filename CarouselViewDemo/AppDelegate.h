b2ec5780ce7528c1a6c08b099078777869f35157
zh <icysol@qq.com>
2016-03-10 16:43:16 +0800

    Initial Commit

---
+++ CarouselViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CarouselViewDemo
+//
+//  Created by zh on 16/3/10.
+//  Copyright © 2016年 zh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CarouselViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CarouselViewDemo
+//
+//  Created by zh on 16/3/10.
+//  Copyright © 2016年 zh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
